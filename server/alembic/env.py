1aecff5e709a4283ba2717ded5f3aa294ecfb64d
Jay17423 <jayprakash1200@gmail.com>
2025-08-29 15:58:34 +0530

    "Added req.txt"

---
+++ server/alembic/env.py
@@ -0,0 +1,68 @@
+from logging.config import fileConfig
+import os
+from dotenv import load_dotenv
+from alembic import context
+from sqlalchemy import engine_from_config, pool
+from sqlmodel import SQLModel
+
+
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL").replace("%", "%%")
+
+# Alembic Config object
+config = context.config
+
+# ✅ Inject DB url into config
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
+
+# Logging setup
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+# ✅ Import your models here
+from app.models.user import User
+from app.models.post import Post
+
+
+# ✅ Metadata for autogenerate
+target_metadata = SQLModel.metadata
+
+
+def run_migrations_offline() -> None:
+    """Run migrations in 'offline' mode."""
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+        compare_type=True,  # detect column type changes
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online() -> None:
+    """Run migrations in 'online' mode."""
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section, {}),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+            compare_type=True,  # detect column type changes
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
